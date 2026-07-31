5b10acabb9dc0b3f2fda2d23fe710d8d73d20567
prateekkumar <prateekkumar@cdac.in>
2026-07-31 16:34:31 +0530

    done changes

--- target/generated-sources/annotations/com/dsa/manager/mapper/TopicMapperImpl.java
+++ target/generated-sources/annotations/com/dsa/manager/mapper/TopicMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-07T16:19:35+0530",
+    date = "2026-07-31T11:33:07+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.18 (Microsoft)"
 )
 @Component
